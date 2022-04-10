b1acdf5f3b9670aaf8a07c64461d87c61987fef2
David Lopez <dflindc@yahoo.com>
2022-04-10 17:51:02 -0400

    contracts ideas
    Added a couple of contracts to learn how this repo works for compiling and deploying, as well as to start thinking about ERC1155 and the general mechanics of an auction contract.
    
    `Auction.sol` is a contract which handles the owner, duration, bidding, winning and ability to withdrawl after an auction. I tested the logic locally and it seems ok. Its not in the right ERC1155 spec.. I was just seeing how I could define an auction.
    
    `GameItems.sol` is taken from a hello world ERC1155 contract from here:
    https://docs.openzeppelin.com/contracts/4.x/erc1155
    
    uses the helper interface/module from OpenZeppelin to get all the functionality. I haven't done much with it other than compile and deploy it.
    
    Some good resources I've found:
    https://docs.openzeppelin.com/contracts/4.x/api/token/erc1155
    https://ethereum.org/en/developers/docs/standards/tokens/erc-1155/
    
    another reference implementation:
    https://github.com/enjin/erc-1155/blob/master/contracts/ERC1155.sol
    
    Doesn't matter if we merge this or not, but I wanted to share.

---
+++ contracts/Auction.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract Auction{
+    // who started auction, and will get paid
+    address payable public owner;
+    uint public startBlock;
+    uint public endBlock;
+
+    //off-chain info about item -- on IPFS
+    //keep pointer to IPFS hash
+    string public ipfsHash;
+
+    enum State {Started, Running, Ended, Canceled}
+    State public auctionState;
+
+    uint public highestBindingBid;
+    address payable public highestBidder;
+
+    // keeping addresses, amounts of bidders in a map
+    mapping (address => uint) public bids;
+
+    // bit increments
+    uint bidIncrement;
+
+
+    // auction is owneed by whomever initiates the contract
+    constructor() {
+        owner = payable(msg.sender);
+        auctionState = State.Running;
+
+        // AUCTION DURATION -- declare how many blocks to allow until auction stops
+        // 24 hr auction, so need current block plus a day's worth of blocks
+        // a block created about every 15 sec, so 86400 / 15 sec is a day
+        startBlock = block.number;
+        endBlock = startBlock + 5760;
+        ipfsHash = "";
+
+        // bid increment in wei
+        bidIncrement = 100;
+    }
+
+
+    // modifiers to enforce some business rules
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+    modifier notOwner(){
+        require(msg.sender != owner);
+        _;
+    }
+
+    modifier afterStart(){
+        require(block.number >= startBlock);
+        _;
+    }
+
+    modifier beforeEnd(){
+        require(block.number <= endBlock);
+        _;
+    }
+
+    // figure out the min of two bids
+    function min(uint x, uint y) pure internal returns (uint){
+        if (x < y){
+            return x;
+        }
+        else {
+            return y;
+        }
+    }
+
+
+    function placeBid() public payable notOwner afterStart beforeEnd {
+        require(auctionState == State.Running);
+        require(msg.value >= bidIncrement);
+
+        // calculate currentBid, ensure its big enough
+        uint currentBid = bids[msg.sender] + msg.value;
+        require(currentBid > highestBindingBid);
+
+        bids[msg.sender] = currentBid;
+
+        // is this bid now highest biding, and greater than current highest bidder?
+        if(currentBid <= bids[highestBidder]){
+            highestBindingBid = min(currentBid + bidIncrement, bids[highestBidder]);
+        } else {
+            highestBindingBid = min(currentBid, bids[highestBidder] + bidIncrement);
+            highestBidder = payable(msg.sender);
+        }
+    }
+
+    // allowing owner to cancel
+    function cancelAuction() public onlyOwner{
+        auctionState = State.Canceled;
+        // need to allow withdrawl for accounts if auction canceled or account not win at auction Ended
+    }
+
+    function finalizeAuction() public {
+        require(auctionState == State.Canceled || block.number > endBlock);
+        require(msg.sender == owner || bids[msg.sender] > 0);
+
+        address payable recipient;
+        uint value;
+
+        // either auction was canceled, or auction ended and have highestbindingbid
+        if (auctionState == State.Canceled){
+            recipient = payable(msg.sender);
+            value = bids[msg.sender];
+        } else {
+            // auction ended, not canceled 
+            // owner gets highest binding bid
+            if (msg.sender == owner){
+                recipient = owner;
+                value = highestBindingBid;
+            } else {
+                // bidder if highest, should get difference between their
+                // highest bid and the final sale price
+                if(msg.sender == highestBidder){
+                    recipient = highestBidder;
+                    value = bids[highestBidder] - highestBindingBid;
+                } else {
+                    // one of the bidders who lost the auction
+                    recipient = payable(msg.sender);
+                    value = bids[msg.sender];
+                }
+            }
+        }
+
+        recipient.transfer(value);
+
+    }
+
+
+}
