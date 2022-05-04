ff47141ac15b33278cd135fe928b0b4c1a2a6068
shivangiuw <94194509+shivangiuw@users.noreply.github.com>
2022-05-03 19:45:16 -0700

    Sgupta (#4)
    * initial code
    
    * initial code
    
    * initial uri

---
+++ contracts/ERC1155Items.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: Unlicense
+
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+
+
+
+contract PreciousStones is ERC1155{
+    uint public DIAMOND = 0;
+    uint public SAPPHIRE =1;
+    uint public EMERALD = 2;
+    uint public RUBY = 3;
+
+    constructor() public ERC1155 (ipfs://bafybeifyp3xgvyszfnd2zipkekw2j6a7sve6rjdzbek7po7gdq4jshsmje/{id}.json){
+        _mint (msg.sender, DIAMOND, 1, " ");
+        _mint (msg.sender, SAPPHIRE, 1, " ");
+        _mint (msg.sender, EMERALD, 1, " ");
+        _mint (msg.sender, RUBY, 1, " ");
+    }
+
+}
+
