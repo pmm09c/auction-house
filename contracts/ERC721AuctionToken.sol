82854e3e33ea9f09f84106b73f0b16ff70917c0d
Rupika Ranjan <rupika@Rupikas-MacBook-Air.local>
2022-05-05 10:55:38 -0700

    adding URI to ERC721

--- contracts/ERC721AuctionToken.sol
+++ contracts/ERC721AuctionToken.sol
@@ -1,15 +1,37 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ERC721AuctionToken is ERC721{
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    mapping (uint256 => string) private _tokenURIs;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol){
         
     }
 
-    
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        require(_exists(tokenId), "ERC721AuctionToken: Setting URI for nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function mintToken(address owner, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+        _mint(owner, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        return newTokenId;
+    }
     
 }
