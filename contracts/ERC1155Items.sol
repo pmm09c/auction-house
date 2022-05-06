da780f0cc8947dd982a056717b0634cdc8f6a813
David L <dflindc@yahoo.com>
2022-05-05 23:04:55 -0400

    erc1155 fixes (#13)

--- contracts/ERC1155Items.sol
+++ contracts/ERC1155Items.sol
@@ -10,16 +10,16 @@ contract G6Items is ERC1155{
     string public name = "G6 Fintech";
     string public symbol= "G6";
 
-    uint public G6 = 0;
-    uint public DL= 1;
-    uint public PM = 2;
-    uint public RRB = 3;
-    uint public JA = 4;
-    uint public SG = 5;
+    uint public G6 = 1;
+    uint public DL= 2;
+    uint public PM = 3;
+    uint public RRB = 4;
+    uint public JA = 5;
+    uint public SG = 6;
 
 
     constructor() public ERC1155 ("ipfs://bafybeibue5ynry7fudeomqxevhmbzfmvho7ya5ogxyasdhhimngwmh2ucq/{id}.json"){
-        _mint (msg.sender, G6, 10**10, " ");
+        _mint (msg.sender, G6, 100, " ");
         _mint (msg.sender, DL, 1, " ");
         _mint (msg.sender, PM, 1, " ");
         _mint (msg.sender, RRB, 1, " ");
