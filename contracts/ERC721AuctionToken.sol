93a17047ceebf459ca90e1ccc1298bf2114de8ec
Rupika Ranjan <rupika@Rupikas-MacBook-Air.local>
2022-05-05 19:44:50 -0700

    Adding functions to set URI and get tokenID

--- contracts/ERC721AuctionToken.sol
+++ contracts/ERC721AuctionToken.sol
@@ -10,7 +10,15 @@ contract ERC721AuctionToken is ERC721{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping (uint256 => string) private _tokenURIs;
+    uint256 currentTokenID;
+
+    mapping (uint256 => string) public _tokenURIs;
+
+    event tokenCreated(
+        address indexed owner,
+        uint tokenId,
+        string uri
+    );
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol){
         
@@ -22,7 +30,7 @@ contract ERC721AuctionToken is ERC721{
     }
 
     function mintToken(address owner, string memory tokenURI)
-        public onlyOwner
+        public  
         returns (uint256)
     {
         _tokenIds.increment();
@@ -31,7 +39,31 @@ contract ERC721AuctionToken is ERC721{
         _mint(owner, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
 
+        // emit tokenCreated(
+        //     owner,
+        //     newTokenId,
+        //     tokenURI
+        // );
+
+        console.log("ERC721AuctionToken : token id is :");
+        console.log(newTokenId);
+        currentTokenID = newTokenId;
         return newTokenId;
+    }
+
+    function getCurrentTokenID() public view returns (uint256){
+
+        console.log("ERC721AuctionToken : Current tokenID :");
+        console.log(currentTokenID);
+        return currentTokenID;
+    }
+
+    function tokenURI(uint256 _tokenId) public view virtual override returns(string memory) {
+        require(
+            _exists(_tokenId),
+            "ERC721AuctionToken: URI set of nonexistent token"
+        );
+        return _tokenURIs[_tokenId];
     }
     
 }
